f5170c72d18fbcecc06c15bd435762a69850e7d1
Chandrakana Nandi <chandra.nandi90@gmail.com>
2025-03-12 23:38:51 -0700

    fix pool status rules WIP

--- pool/src/spec/pool_status_rules.rs
+++ pool/src/spec/pool_status_rules.rs
@@ -73,7 +73,14 @@ pub fn verify_update_status_0_b(e: Env) {
 
     let status_after = execute_update_pool_status(&e);
 
-    cvlr_assert!(unsafe { GHOST_POOL_BACKSTOP_DATA.q4w_pct < 0_6000000 } || status_after == 5);
+    // if (q4w >= 60 && !(q4w >= 50 || !met_threshold)) then status = 5
+    // !(q4w >= 60 && !(q4w >= 50 || !met_threshold)) || (status = 5)
+    // (q4w < 60 || (q4w >= 50 || !met_threshold)) || (status = 5)
+    // (q4w < 60 || q4w >= 50 || !met_threshold || (status = 5)
+
+    cvlr_assert!(unsafe { GHOST_POOL_BACKSTOP_DATA.q4w_pct < 0_6000000 } ||
+                 unsafe { GHOST_POOL_BACKSTOP_DATA.q4w_pct >= 0_5000000 } ||
+                 unsafe { !GHOST_MET_THRESHOLD } || status_after == 5);
 }
 
 #[rule]
@@ -82,14 +89,29 @@ pub fn verify_update_status_0_c(e: Env) {
 
     cvlr_assume!(pool_config.status == 0);
 
+
     storage::set_pool_config(&e, &pool_config);
+
+    clog!(pool_config.status);
 
     let status_after = execute_update_pool_status(&e);
 
+    unsafe{
+        clog!(GHOST_MET_THRESHOLD);
+        clog!((GHOST_POOL_BACKSTOP_DATA.q4w_pct >> 64) as i64);
+        clog!(GHOST_POOL_BACKSTOP_DATA.q4w_pct as i64);
+    }
+    clog!(status_after);
+
+    // if (!q4w >= 60 && !(q4w >= 50 || !met_threshold) && (q4w >= 30 || !met_threshold)) then status = 3
+    //   !(!q4w >= 60 && !(q4w >= 50 || !met_threshold) && (q4w >= 30 || !met_threshold)) || (status = 3)
+    //   !(q4w < 60   && q4w < 50 && met_threshold && (q4w >= 30 || !met_threshold)) || (status = 3)
+
     cvlr_assert!(
-        (unsafe { GHOST_MET_THRESHOLD } && unsafe { GHOST_POOL_BACKSTOP_DATA.q4w_pct < 0_3000000 })
-            || status_after == 3
-    );
+            !(unsafe {GHOST_POOL_BACKSTOP_DATA.q4w_pct < 0_6000000} &&
+              unsafe {GHOST_POOL_BACKSTOP_DATA.q4w_pct < 0_5000000} &&
+              (unsafe {GHOST_POOL_BACKSTOP_DATA.q4w_pct >= 0_3000000} || !unsafe {GHOST_MET_THRESHOLD}) &&
+              unsafe {GHOST_MET_THRESHOLD})|| status_after == 3);
 }
 
 #[rule]
@@ -145,11 +167,25 @@ pub fn verify_update_status_other_b(e: Env) {
     );
 
     storage::set_pool_config(&e, &pool_config);
+    clog!(pool_config.status);
 
     let status_after = execute_update_pool_status(&e);
 
+    unsafe{
+        clog!(GHOST_MET_THRESHOLD);
+        clog!((GHOST_POOL_BACKSTOP_DATA.q4w_pct >> 64) as i64);
+        clog!(GHOST_POOL_BACKSTOP_DATA.q4w_pct as i64);
+    }
+    clog!(status_after);
+
+    // if q4w < 60 && (q4w >= 30 || !met_threshold) then status = 3
+    // !(q4w < 60 && (q4w >= 30 || !met_threshold)) || (status = 3)
+    // (q4w >= 60 || !(q4w >= 30 || !met_threshold)) || (status = 3)
+    // (q4w >= 60 || (q4w < 30 && met_threshold)) || (status = 3)
+
     cvlr_assert!(
-        (unsafe { GHOST_MET_THRESHOLD } && unsafe { GHOST_POOL_BACKSTOP_DATA.q4w_pct < 0_3000000 })
+        ((unsafe { GHOST_MET_THRESHOLD } && unsafe { GHOST_POOL_BACKSTOP_DATA.q4w_pct < 0_3000000 }) ||
+          unsafe { GHOST_POOL_BACKSTOP_DATA.q4w_pct >= 0_6000000})
             || status_after == 3
     );
 }
