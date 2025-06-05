c88ac0eff0e62aa239a9a086ae979fe9cc1d7fc8
Chandrakana Nandi <chandra.nandi90@gmail.com>
2025-06-05 12:40:20 -0700

    update rules after blend docs were fix

--- pool/src/spec/pool_status_rules.rs
+++ pool/src/spec/pool_status_rules.rs
@@ -19,7 +19,7 @@ pub fn verify_status_update(e: Env) {
 
     clog!(status_after);
 
-    cvlr_assert!(status_after == 1 || status_after == 3 || status_after == 5);
+    cvlr_assert!(status_after == 1 || status_after == 3 || status_after == 5 || status_after == 0 || status_after == 2);
 }
 
 #[rule]
@@ -76,15 +76,7 @@ pub fn verify_update_status_0_a(e: Env) {
     cvlr_assert!(status_after == 3);
 }
 
-/**
- * NOTE that this rule is directly taken from
- * the official documentation: https://docs.blend.capital/tech-docs/core-contracts/lending-pool/pool-management#permissionless-updates
- * However, as you can see below,
- * the documentation does not make sense because it is not possible
- * for GHOST_POOL_BACKSTOP_DATA.q4w_pct < 0_5000000 and also >= 0_6000000.
- * This rule will therefore always pass but it is vacuous.
- * Please consider updating the official documentation.
- */
+
 #[rule]
 pub fn verify_update_status_0_b(e: Env) {
     let pool_config: PoolConfig = cvlr::nondet();
@@ -96,44 +88,9 @@ pub fn verify_update_status_0_b(e: Env) {
     let status_after = execute_update_pool_status(&e);
 
     cvlr_assume!(!(unsafe {GHOST_POOL_BACKSTOP_DATA.q4w_pct >= 0_5000000} || unsafe { !GHOST_MET_THRESHOLD }));
-    cvlr_assume!(unsafe {GHOST_POOL_BACKSTOP_DATA.q4w_pct >= 0_6000000});
-
-    cvlr_assert!(status_after == 5);
+    cvlr_assert!(status_after == 0);
 }
 
-#[rule]
-pub fn verify_update_status_0_c(e: Env) {
-    let pool_config: PoolConfig = cvlr::nondet();
-
-    cvlr_assume!(pool_config.status == 0);
-
-    storage::set_pool_config(&e, &pool_config);
-
-    let status_after = execute_update_pool_status(&e);
-
-    cvlr_assume!(!(unsafe {GHOST_POOL_BACKSTOP_DATA.q4w_pct >= 0_5000000} || unsafe { !GHOST_MET_THRESHOLD }));
-    cvlr_assume!(!unsafe {GHOST_POOL_BACKSTOP_DATA.q4w_pct >= 0_6000000});
-    cvlr_assume!(unsafe {GHOST_POOL_BACKSTOP_DATA.q4w_pct >= 0_3000000} || unsafe { !GHOST_MET_THRESHOLD });
-
-    cvlr_assert!(status_after == 3);
-}
-
-#[rule]
-pub fn verify_update_status_0_d(e: Env) {
-    let pool_config: PoolConfig = cvlr::nondet();
-
-    cvlr_assume!(pool_config.status == 0);
-
-    storage::set_pool_config(&e, &pool_config);
-
-    let status_after = execute_update_pool_status(&e);
-
-    cvlr_assume!(!(unsafe {GHOST_POOL_BACKSTOP_DATA.q4w_pct >= 0_5000000} || unsafe { !GHOST_MET_THRESHOLD }));
-    cvlr_assume!(!unsafe {GHOST_POOL_BACKSTOP_DATA.q4w_pct >= 0_6000000});
-    cvlr_assume!(!(unsafe {GHOST_POOL_BACKSTOP_DATA.q4w_pct >= 0_3000000} || unsafe { !GHOST_MET_THRESHOLD }));
-
-    cvlr_assert!(status_after == 1);
-}
 
 #[rule]
 pub fn verify_update_status_other_a(e: Env) {
